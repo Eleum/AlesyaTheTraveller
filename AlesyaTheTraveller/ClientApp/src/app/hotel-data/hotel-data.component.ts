f406e39cfbc02ff8de57fafd962e69f9d9f5591c
Dmitry Vasilevich <wotlk777@gmail.com>
2019-06-05 12:30:25 +0300

    responsive layout

--- AlesyaTheTraveller/ClientApp/src/app/hotel-data/hotel-data.component.ts
+++ AlesyaTheTraveller/ClientApp/src/app/hotel-data/hotel-data.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription } from 'rxjs';
 import { SignalRService } from '../services/signal-r.service';
 import { HotelData } from './hotel-data.model';
 import { ToastrService } from 'ngx-toastr';
+import { PaginatorService } from '../services/paginator.service';
 
 @Component({
   selector: 'app-hotel-data',
@@ -15,13 +16,19 @@ export class HotelDataComponent implements OnInit, AfterViewInit, OnDestroy {
   private subscription: Subscription;
   private notifySubscription: Subscription;
 
-  constructor(private signalr: SignalRService, private toastr: ToastrService) { }
+  private hd: HotelData[];
+  private paginator: any = {};
+  private pageItems: any[];
+
+  constructor(private signalr: SignalRService, private toastr: ToastrService, private paginatorService: PaginatorService) { }
 
   ngOnInit() {
     this.hotelData = this.signalr.hotelDataFetched.asObservable();
     this.subscription = this.hotelData.subscribe(data => {
       if (data != undefined && data.length > 0) {
         this.hotelText = "Вот список мест для проживания. Выбирайте любое."
+        this.hd = data;
+        this.setPage(1);
       }
       console.log(data);
     });
@@ -48,5 +55,13 @@ export class HotelDataComponent implements OnInit, AfterViewInit, OnDestroy {
 
   getHotelClass(hotel: HotelData) {
     return new Array(hotel.Class);
+  }
+
+  setPage(page: number) {
+    // get paginator object
+    this.paginator = this.paginatorService.getPaginator(this.hd.length, page);
+
+    // get items for specified page
+    this.pageItems = this.hd.slice(this.paginator.startIndex, this.paginator.endIndex + 1);
   }
 }
